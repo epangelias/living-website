a835c7336d302591e9362013145fb0f38270c4c1
Epangelias <epangealis@gmail.com>
2024-06-16 16:54:35 -0700

    fix to generator

--- routes/sitemap.xml.tsx
+++ routes/sitemap.xml.tsx
@@ -14,6 +14,10 @@ export async function handler(req: Request) {
 
   return new Response(`<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${pages.map((page) => `<url><loc>${page}</loc></url>`).join("")}
+  ${
+    pages.map((page) =>
+      `<url><loc>https://living.vaza.top/${page.path}</loc></url>`
+    ).join("\n")
+  }
     `);
 }
