d7afe3fe2c587e65571673f243a5e07f79cd4b11
Epangelias <epangealis@gmail.com>
2024-06-15 21:20:40 -0700

    ch favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,18 @@
 import { type PageProps } from "$fresh/server.ts";
+
+export function emojiToUrl(emoji: string) {
+  return `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`;
+}
+
 export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>living-website</title>
+        <title>Living Website</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="shortcut icon" href={emojiToUrl("⚡")} type="image/svg" />
         {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css"> */}
         {/* <meta name="color-scheme" content="light dark" /> */}
       </head>
