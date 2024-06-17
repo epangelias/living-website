023a55b8933c6818537489595e4f8667afda660c
Epangelias <epangealis@gmail.com>
2024-06-16 22:33:14 -0700

    fix filter sitemap

--- routes/sitemap.xml.tsx
+++ routes/sitemap.xml.tsx
@@ -1,8 +1,9 @@
 import { GetPages } from "../lib/path.ts";
 
 export async function handler(req: Request) {
-  const pages = await GetPages();
-
+  const pages = (await GetPages()).filter((p) =>
+    !p.path.toLocaleLowerCase().includes("jaden")
+  );
   return new Response(
     `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${
       pages.map((page) =>
