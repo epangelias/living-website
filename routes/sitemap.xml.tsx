0c979a78624b5ef2aef3399e5889ba72cbde6f31
Epangelias <epangealis@gmail.com>
2024-06-16 17:09:15 -0700

    fix sitemap

--- routes/sitemap.xml.tsx
+++ routes/sitemap.xml.tsx
@@ -1,26 +1,13 @@
-import { db } from "../lib/db.ts";
 import { GetPages } from "../lib/path.ts";
 
 export async function handler(req: Request) {
   const pages = await GetPages();
 
-  pages.forEach((page) => {
-    if (
-      page.path.toLowerCase().includes("herman") ||
-      page.path.toLowerCase().includes("jaden") ||
-      page.path.toLowerCase().includes("dan") ||
-      page.path.toLowerCase().includes("gay") ||
-      page.path.toLowerCase().includes("jesus")
-    ) db.delete(["pages", page.path]);
-    console.log(page.path);
-  });
-
-  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${
-    pages.map((page) =>
-      `<url><loc>https://living.vaza.top/${page.path}</loc></url>`
-    ).join("\n")
-  }
-    `);
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${
+      pages.map((page) =>
+        `<url><loc>https://living.vaza.top/${page.path}</loc></url>`
+      ).join("\n")
+    }\n</urlset>`,
+  );
 }
